897fb0219a1719dbe4f50971a2cc360e8b86140a
Sheehan Shams <s2shams@uwaterloo.ca>
2023-03-15 21:25:18 -0400

    added omdb api functionality

--- main.py
+++ main.py
@@ -11,12 +11,41 @@ import json
 # Get user input and relevant information:
 jikan = Jikan()
 
-user_movies = input("Enter 5 movies you like (separated by commas): ").split(",")
+user_title = input("Enter 5 movies you like (separated by commas): ").split(",")
 user_genre = []
 user_synopsis = []
-user_title = []
+
 # Use Jikan and IMDb/OMDb APIs above to extract relevant info
 
+for name in user_title:
+    url = 'http://www.omdbapi.com/?t=' + name.replace(' ', '+') + '&plot=full' + '&apikey=' + API_KEY + '&'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            synopsis = data['Plot']
+            user_synopsis.appennd(synopsis)
+            
+            genre = data['Genre']
+            user_genre.append(genre)
+            
+        except KeyError:
+            user_genre.append('none')
+            user_synopsis.append('none')
+            
+
+# if the lists have 'none' in them, then the user probably inputted an anime that isnt in OMDb
+
+for index, name in enumerate(user_genre):
+    if name != 'none':
+        continue
+    else:
+        anime = user_title[index]
+        # JIKAN API functionality problem, limitation is that user must search using name
+        # as how it appears in MAL which is usually its japanese title.
+        
+        
 # Create existing dataframe and vectorize it:
 df = pd.read_csv('df.csv')
 vectorize_df = df.drop(['title', 'score'], axis=1)
@@ -33,5 +62,5 @@ vectorized_df = pd.DataFrame(combined_array, columns=feature_names)
 user_df = df.copy()
 user_df.drop(index=user_df.index, inplace=True)
 columns = user_df.columns.tolist()
-# i=git test
+
 
